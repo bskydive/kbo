7e642760b672e424b1f8f69c2ffe463f694825ac
Valeriy Stepanov <stepanovv.ru@yandex.ru>
2024-07-09 16:40:37 +0300

    scripts

--- remote_git_init.sh
+++ remote_git_init.sh
@@ -7,5 +7,7 @@ repo_name=kbo
 cat .git/config | grep -i url
 
 git remote add gl git@gitlab.com:stepanovv/${repo_name}.git && git remote remove origin
+git branch --set-upstream-to=gl/master master
+git branch --set-upstream-to=gl/develop develop
 git remote add gh git@github.com:bskydive/${repo_name}.git
 
